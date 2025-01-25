5fe88bdee08f47c69f91d44a0bb9576bb5543e2c
AdrienW86 <adrien_weiss@outlook.fr>
2025-01-25 23:13:28 +0100

    css

--- components/Description/Description.js
+++ components/Description/Description.js
@@ -4,31 +4,38 @@ import styles from './description.module.css'
 export default function Description() {
   return (
     <section className={styles.description}>
-         <h2 className={styles.h2}> Localisation </h2>
+      <h2 className={styles.h2}> Localisation </h2>
        <div className={styles.intro}>
-        Nichée au cœur de la Gascogne, entre collines verdoyantes 
-        et paysages vallonnés, le Château de Projan vous ouvre ses 
-        portes pour une escapade hors du temps. Cet élégant domaine, 
-        chargé d'histoire et de charme, offre un cadre idyllique pour 
-        les amoureux de nature, de sérénité et d'authenticité.
-
-Dès votre arrivée, vous serez captivé par l'architecture raffinée du château, sublimée par des détails historiques soigneusement préservés. Chaque pièce raconte une histoire, mêlant élégance classique et confort moderne. Les chambres spacieuses et lumineuses, décorées avec goût, offrent une vue imprenable sur les jardins et les collines environnantes.
+          Nichée au cœur de la Gascogne, entre collines verdoyantes 
+          et paysages vallonnés, le Château de Projan vous ouvre ses 
+          portes pour une escapade hors du temps. Cet élégant domaine, 
+          chargé d'histoire et de charme, offre un cadre idyllique pour 
+          les amoureux de nature, de sérénité et d'authenticité.
+          Dès votre arrivée, vous serez captivé par l'architecture raffinée
+          du château, sublimée par des détails historiques soigneusement préservés.
+          Chaque pièce raconte une histoire, mêlant élégance classique et confort moderne. 
+          Les chambres spacieuses et lumineuses, décorées avec goût, offrent une vue
+          imprenable sur les jardins et les collines environnantes.
        </div>
-       <h2 className={styles.h2}>  Pourquoi séjourner au Château de Projan ? </h2>
-        <div className={styles.article}>
-       
-Un cadre enchanteur : Profitez d’un parc majestueux où il fait bon se promener, lire ou simplement se ressourcer.
-Une gastronomie régionale : Découvrez les saveurs authentiques de la Gascogne avec des produits locaux et une cuisine généreuse.
-Des activités variées : Balades à vélo, visites de vignobles, découverte des villages pittoresques et des marchés traditionnels de la région.
-Bien-être et détente : Laissez-vous tenter par un moment de relaxation sur la terrasse ensoleillée ou près de la piscine, dans une atmosphère paisible.
-Une destination pour tous
-Que vous soyez en quête d'une escapade romantique, d'un séjour en famille ou d’un lieu de séminaire unique, le Château de Projan saura répondre à vos attentes. Chaque détail a été pensé pour que votre séjour soit inoubliable.
-
-Venez découvrir le charme et l’authenticité du Château de Projan, un lieu où passé et présent s’entrelacent pour vous offrir une expérience unique. Réservez dès aujourd’hui pour vivre un moment d'exception dans un cadre enchanteur.
-
-Votre séjour de rêve vous attend... 🌟
+      <h2 className={styles.h2}>  Pourquoi séjourner au Château de Projan ? </h2>
+        <div className={styles.article}>      
+          Un cadre enchanteur : Profitez d’un parc majestueux où il fait bon se promener, 
+          lire ou simplement se ressourcer.
+          Une gastronomie régionale : Découvrez les saveurs authentiques de la Gascogne
+          avec des produits locaux et une cuisine généreuse.
+          Des activités variées : Balades à vélo, visites de vignobles,
+          découverte des villages pittoresques et des marchés traditionnels de la région.
+          Bien-être et détente : Laissez-vous tenter par un moment de relaxation sur la 
+          terrasse ensoleillée ou près de la piscine, dans une atmosphère paisible.
+          Une destination pour tous
+          Que vous soyez en quête d'une escapade romantique, d'un séjour en famille 
+          ou d’un lieu de séminaire unique, le Château de Projan saura répondre à vos attentes.
+          Chaque détail a été pensé pour que votre séjour soit inoubliable.
+          Venez découvrir le charme et l’authenticité du Château de Projan, un lieu où passé et 
+          présent s’entrelacent pour vous offrir une expérience unique. Réservez dès aujourd’hui 
+          pour vivre un moment d'exception dans un cadre enchanteur.
+          Votre séjour de rêve vous attend... 🌟
         </div>
-
     </section>
   )
 }
