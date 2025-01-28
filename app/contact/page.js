9fc2fa178151fd1197eb8d70caa4eb5150f22e99
AdrienW86 <adrien_weiss@outlook.fr>
2025-01-29 00:37:52 +0100

    mise en ligne

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import Gallery from '@/components/Gallery/Gallery'
+import Header from '@/components/Header/Header'
+import Map from '@/components/Map/Map'
+import Footer from '@/components/Footer/Footer'
+import styles from '../page.module.css'
 
-export default function page() {
+
+export default function Contact() {
   return (
-    <>
-    <Gallery />
-    </>
+    <main className={styles.main}>
+      <Header />
+     <h2 className={styles.h2}>  Nous contacter </h2>
+      <Map />
+      <Footer />
+
+     
+    </main>
   )
 }
