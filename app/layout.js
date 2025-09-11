880ba4b3a4f2bed633e599b01fd7515a6f5aa9ff
AdrienW86 <adrien_weiss@outlook.fr>
2025-09-11 16:23:25 +0200

    mise en ligne

--- app/layout.js
+++ app/layout.js
@@ -5,20 +5,18 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Château de Projan',
-  description: "Venez vivre une expérience inoubliable au Château de Projan, situé près de Pau. Niché au cœur de la nature, ce château moderne allie confort et élégance. Profitez d'un séjour dans un cadre unique avec des chambres spacieuses et une vue imprenable. Parfait pour une escapade romantique ou un moment de détente. Réservez dès maintenant pour une expérience exceptionnelle !"
+  description: "Venez vivre une expérience inoubliable au Château de Projan, situé près de Pau...",
+  icons: {
+    icon: '/favicon2.png',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-       <head>
-        <link rel="icon" href="favicon2.png" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
